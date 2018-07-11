82015bc8a4e8366169dbf7f8ba19a9d1db6c1b07
hems <hems.inlet@gmail.com>
2018-07-11 15:42:38 +0100

    updating ETH lock call

--- src/api/contract/lock.js
+++ src/api/contract/lock.js
@@ -9,22 +9,22 @@ module.exports = async (efx, token, amount, duration) => {
 
   const action = 'deposit'
 
-  // In order to lock ETH we simply send ETH to the lockerAddress
-  if (token === 'ETH') {
-    return efx.eth.send(
-      efx.contract.abi.weth,
-      currency.lockerAddress,
-      action,
-      [],
-      value
-    )
-  }
-
   // In order to lock tokens we call deposit with value and forTime
   const args = [
     value, // uint256 value
     duration // uint256 forTime
   ]
+
+  // In order to lock ETH we simply send ETH to the lockerAddress
+  if (token === 'ETH') {
+    return efx.eth.send(
+      efx.contract.abi.locker,
+      currency.lockerAddress,
+      action,
+      args,
+      value // send ETH to the contract
+    )
+  }
 
   return efx.eth.send(
     efx.contract.abi.locker,
