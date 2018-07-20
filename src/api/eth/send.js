c80e7c567b94b73ebb85c3a646efdec3b8e1a50c
hems <hems.inlet@gmail.com>
2018-07-20 17:07:55 +0100

    estimating gas before calling .send on method

--- src/api/eth/send.js
+++ src/api/eth/send.js
@@ -1,10 +1,17 @@
-module.exports = (efx, abi, address, action, args, value = 0) => {
+module.exports = async (efx, abi, address, action, args, value = 0) => {
   const { web3 } = efx
 
   const contract = new web3.eth.Contract(abi, address)
 
-  return contract.methods[action](...args).send({
+  const method = contract.methods[action](...args)
+
+  const estimatedGas = await method.estimateGas({
+    from: efx.config.account
+  })
+
+  return method.send({
     from: efx.config.account,
-    value: value
+    value: value,
+    gas: estimatedGas
   })
 }
