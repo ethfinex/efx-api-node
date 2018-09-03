25c51523880fe32c578dc6187d23e6550b8a71eb
hems <hems.inlet@gmail.com>
2018-09-03 19:41:10 +0100

    making sure no more than 1 request is done at once

--- examples/node_spam_submit_orders.js
+++ examples/node_spam_submit_orders.js
@@ -28,6 +28,32 @@ submitSellOrder = async (efx, account, price) => {
   }
 }
 
+cancelAllOrders = async(efx, account) => {
+
+  await efx.account.select(account)
+
+  await efx.account.unlock('password')
+
+  // Cancel all orders
+  response = await efx.getOrders()
+
+  console.log(`Found ${response.length} orders from ${account}`)
+
+  for(const order of response){
+    console.log("")
+
+    console.log( "cancel order #", order.id )
+
+    try {
+      response = await efx.cancelOrder(order.id)
+      console.log(" - OK")
+    } catch(e) {
+      //console.log( 'e ->', e)
+      console.log( "error:", e.response.body )
+    }
+  }
+}
+
 work = async () => {
   // assuming you have a provider running at:
   // http://localhost:8545
@@ -41,19 +67,26 @@ work = async () => {
 
   const INITIAL_PRICE = 1000
 
-  // send 5 orders in a row
-  const sells = Promise.all([
-    submitSellOrder(efx, accounts[indexes[1]], INITIAL_PRICE + 0 ),
-    submitSellOrder(efx, accounts[indexes[2]], INITIAL_PRICE + 1 ),
-    submitSellOrder(efx, accounts[indexes[3]], INITIAL_PRICE + 2 ),
-    submitSellOrder(efx, accounts[indexes[4]], INITIAL_PRICE + 3 ),
-    submitSellOrder(efx, accounts[indexes[5]], INITIAL_PRICE + 4 )
-  ])
-
-  await sells
+  // send 5 orders, sequentially not in parallel
+  await submitSellOrder(efx, accounts[indexes[0]], INITIAL_PRICE + 0 )
+  await submitSellOrder(efx, accounts[indexes[1]], INITIAL_PRICE + 1 )
+  await submitSellOrder(efx, accounts[indexes[2]], INITIAL_PRICE + 2 )
+  await submitSellOrder(efx, accounts[indexes[3]], INITIAL_PRICE + 3 )
+  await submitSellOrder(efx, accounts[indexes[4]], INITIAL_PRICE + 4 )
 
   console.log( "-------------" )
   console.log( "sent all sells" )
+  console.log( "" )
+
+  await cancelAllOrders(efx, accounts[indexes[0]], INITIAL_PRICE + 0 )
+  await cancelAllOrders(efx, accounts[indexes[1]], INITIAL_PRICE + 1 )
+  await cancelAllOrders(efx, accounts[indexes[2]], INITIAL_PRICE + 2 )
+  await cancelAllOrders(efx, accounts[indexes[3]], INITIAL_PRICE + 3 )
+  await cancelAllOrders(efx, accounts[indexes[4]], INITIAL_PRICE + 4 )
+
+  console.log( "-------------" )
+  console.log( "cancelled all sells" )
+  console.log( "" )
 
 
   return
