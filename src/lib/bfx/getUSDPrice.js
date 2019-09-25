e88ef8bd064f70504b1fb28b03730e35bf673b8f
hems <hems.inlet@gmail.com>
2019-09-25 12:18:45 +0100

    adding TODO

--- src/lib/bfx/getUSDPrice.js
+++ src/lib/bfx/getUSDPrice.js
@@ -1,13 +1,16 @@
 const {post} = require('request-promise')
 const stableCoins = require('./stableCoins')
 
+// TODO: move address to api.deversifi address
+const BFX_API = 'https://api-pub.bitfinex.com/v2'
+
 module.exports = async (token) => {
   if (stableCoins[token]) {
     return stableCoins[token]
   }
 
   const response = await post({
-    url: `https://api-pub.bitfinex.com/v2/calc/fx`,
+    url: BFX_API + `/calc/fx`,
     json: true,
     body: {
       ccy1: token,
