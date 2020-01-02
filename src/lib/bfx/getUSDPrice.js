2e1624b0bb8d05b425c5ded7b976c4647776dc05
tempeer <alimeer@gmail.com>
2020-01-02 15:12:07 +0000

    removed https comnent line

--- src/lib/bfx/getUSDPrice.js
+++ src/lib/bfx/getUSDPrice.js
@@ -1,10 +1,10 @@
-const {post} = require('request-promise')
+const { post } = require('request-promise')
 const stableCoins = require('./stableCoins')
 
 // TODO: move address to api.deversifi address
 const BFX_API = 'https://api.deversifi.com/bfx/v2'
 
-https: module.exports = async token => {
+module.exports = async token => {
   if (stableCoins[token]) {
     return stableCoins[token]
   }
