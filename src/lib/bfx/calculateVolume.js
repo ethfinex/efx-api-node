edad263acf43a6cb78d046105a46c2bb7d1413ad
Mateja Trifunovski <mtrifunovski@gmail.com>
2020-02-14 11:30:35 +0100

    Format calculateVolume pair

--- src/lib/bfx/calculateVolume.js
+++ src/lib/bfx/calculateVolume.js
@@ -1,10 +1,15 @@
 const getUSDPrice = require('./getUSDPrice')
 
 module.exports = async (symbol, amount, price) => {
-  const baseSymbol = symbol.substr(0, symbol.length - 3)
-  const quoteSymbol = symbol.substr(-3)
+  let baseSymbol, quoteSymbol
+  if (_.includes(symbol, ':')) {
+    [baseSymbol, quoteSymbol] = _.split(symbol, ':', 2)
+  } else {
+    baseSymbol = symbol.substr(0, symbol.length - 3)
+    quoteSymbol = symbol.substr(-3)
+  }
 
-  quoteSymbolPrice = await getUSDPrice(quoteSymbol)
+  const quoteSymbolPrice = await getUSDPrice(quoteSymbol)
 
   // long or short the volume will be the same
   amount = Math.abs(amount)
